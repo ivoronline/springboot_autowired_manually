46f09619306d463dfd0b1ed466196a9c936322ea
igoreta <ivoronline@gmail.com>
2020-12-22 16:22:02 +0100

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_autowired_manually/SpringbootAutowiredManuallyApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_autowired_manually;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAutowiredManuallyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootAutowiredManuallyApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_autowired_manually/SpringbootAutowiredManuallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_autowired_manually;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAutowiredManuallyApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
